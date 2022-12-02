b4af264e75469d12609dbaa48331bebb31be1902
Paiboon Sriboonrueng <paiboon.s@uobkayhian.co.th>
2022-12-02 20:20:25 +0700

    add project.

---
+++ iosUfisMessage/iosUfisMessage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosUfisMessage
+//
+//  Created by admin on 2/12/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
